95847fd4e4670823c58ae6c26a5a92949d1ea53f
Tane4ka170 <tane4ka170@gmail.com>
2023-12-23 01:59:19 +0200

    update with commands

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs-homework-01
+
+Screenshots: https://monosnap.com/list/658622532973536925946977
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const contacts = require("./contacts");
+const { Command } = require('commander');
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
     case "get":
       const oneContact = await contacts.getContactById(id);
       console.log(oneContact);
       break;
     case "add":
-      const newContact = await contacts.addContact({ name, email, phone });
+      const newContact = await contacts.addContact(name, email, phone);
       console.log(newContact);
       break;
     case "remove":
@@ -22,3 +35,5 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.log("Unknown action");
   }
 }
+
+invokeAction(argv);
