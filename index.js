af712417dfcbf1e2de342df6fa1cd637f899ac53
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-12-22 18:48:34 +0200

    made contacts json

---
+++ index.js
@@ -0,0 +1,24 @@
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log("Removed contact:", removedContact);
+      break;
+    default:
+      console.log("Unknown action");
+  }
+}
